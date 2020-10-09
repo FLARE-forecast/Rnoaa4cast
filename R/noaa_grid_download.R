245c3620923317925e0dfc1ba322a3860ecaefbe
rqthomas <rqthomas>
2020-10-09 13:38:41 -0400

    Adding check for complete downloads

--- R/noaa_grid_download.R
+++ R/noaa_grid_download.R
@@ -16,8 +16,7 @@
 noaa_grid_download <- function(lat_list, lon_list, forecast_time, forecast_date ,model_name_raw, num_cores, output_directory) {
 
 
-  download_neon_grid <- function(ens_index, location, directory, hours_char, cycle, base_filename1, vars,
-                                 max_file_size, max_file_size_f000,working_directory){
+  download_neon_grid <- function(ens_index, location, directory, hours_char, cycle, base_filename1, vars,working_directory){
     #for(j in 1:31){
     if(ens_index == 1){
       base_filename2 <- paste0("gec00",".t",cycle,"z.pgrb2a.0p50.f")
@@ -39,23 +38,36 @@ noaa_grid_download <- function(lat_list, lon_list, forecast_time, forecast_date 
       destfile <- paste0(working_directory,"/", file_name,".neon.grib")
 
       if(file.exists(destfile)){
-        size <- file.info(destfile)$size
-        if(i == 1){
-          max_size <- max_file_size_f000
-        }else{
-          max_size <- max_file_size
-        }
 
-        if(size == 0 | size <= (0.90 * max_size)){
-          incorrect_size <- TRUE
-        }else{
-          incorrect_size <- FALSE
+        fsz <- file.info(destfile)$size
+        gribf <- file(filename, "rb")
+        fsz4 <- fsz-4
+        seek(gribf,where = fsz4,origin = "start")
+        last4 <- readBin(gribf,"raw",4)
+        if(as.integer(last4[1])==55 & as.integer(last4[2])==55 & as.integer(last4[3])==55 & as.integer(last4[4])==55) {
+          download_file <- FALSE
+        } else {
+          download_file <- TRUE
         }
+        close(gribf)
+
+        #size <- file.info(destfile)$size
+        #if(i == 1){
+        #  max_size <- max_file_size_f000
+        #}else{
+        #  max_size <- max_file_size
+        #}
+
+        #if(size == 0 | size <= (0.90 * max_size)){
+        #  incorrect_size <- TRUE
+        #}else{
+        #  incorrect_size <- FALSE
+        #}
       }else{
-        incorrect_size <- FALSE
+        download_file <- TRUE
       }
 
-      if(!file.exists(destfile) | incorrect_size){
+      if(download_file){
 
         out <- tryCatch(download.file(paste0(base_filename1, file_name, vars, location, directory),
                                       destfile = destfile, quiet = TRUE),
@@ -132,38 +144,10 @@ noaa_grid_download <- function(lat_list, lon_list, forecast_time, forecast_date 
 
       if(cycle < 10) cycle <- paste0("0",cycle)
 
-
       model_date_hour_dir <- file.path(model_dir,forecast_date,cycle)
       if(!dir.exists(model_date_hour_dir)){
         dir.create(model_date_hour_dir, recursive=TRUE, showWarnings = FALSE)
-        max_file_size_f000 <- 0
-        max_file_size <- 0
-
-      }else{
-        f1 <- list.files(model_date_hour_dir, pattern = "f000", full.names = TRUE)
-        f2<- list.files(model_date_hour_dir, full.names = TRUE)
-        max_file_size_f000 <- max(file.info(f1)$size)
-        max_file_size <- max(file.info(f2)$size)
       }
-      #  new_download <- TRUE
-      #}else{
-      #  new_download <- TRUE
-      #  file <- list.files(model_date_hour_dir)
-      #  present_ensemble <- stringr::str_sub(file, start = 4, end = 5)
-      #  present_times <- as.numeric(stringr::str_sub(file, start = 25, end = 27))
-      #  if(length(unique(present_ensemble)) == 31){
-      #    if(cycle == "00"){
-      #      if(max(present_times) == 840 & length(which(present_times == 384)) == 31){
-      #        new_download <- FALSE
-      #      }
-      #      else{
-      #        if( length(which(present_times == 384)) == 31){
-      #          new_download <- FALSE
-      #        }
-      #      }
-      #    }
-      #  }
-      #}
 
       new_download <- TRUE
 
@@ -197,8 +181,6 @@ noaa_grid_download <- function(lat_list, lon_list, forecast_time, forecast_date 
                            cycle,
                            base_filename1,
                            vars,
-                           max_file_size,
-                           max_file_size_f000,
                            working_directory = model_date_hour_dir,
                            mc.cores = num_cores)
       }else{
