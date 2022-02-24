750843631adf0eacd66751ae32728166e5628bce
rqthomas <rqthomas@vt.edu>
2022-02-24 00:05:04 +0000

    fixing cfs s3 download

--- R/noaa_cfs_grid_download.R
+++ R/noaa_cfs_grid_download.R
@@ -11,10 +11,9 @@
 #' most recent date.))
 #' @param model_name_raw A string with the model name used as the root of the downloaded directory
 #' tree.
+#' @param num_cores DEPRECATED
 #' @param output_directory Path: directory where the forecast output and logs will be saved.
-#' @param grid_name A short grid name used in directory and file name generation.
-#' @param s3_mode Logical: save the forecast to a Amazon S3 bucket rather than locally.
-#' @param bucket If s3_mode = TRUE, the S3 bucket name to save to.
+#'
 #' @details
 #' @section Coordinates
 #' ((Copy from noaa_gefs_download_downscale))
@@ -32,15 +31,129 @@
 # - Need to add checking of parameters.
 # - Add a default for model_name_raw?
 
-noaa_cfs_grid_download <- function(lat_list,
-                                   lon_list,
-                                   forecast_time,
-                                   forecast_date,
-                                   model_name_raw,
-                                   output_directory,
-                                   grid_name,
-                                   s3_mode = FALSE,
-                                   bucket = NULL) {
+noaa_cfs_grid_download <- function(lat_list, lon_list, forecast_time, forecast_date, model_name_raw, output_directory, grid_name) {
+
+
+  download_grid <- function(forecasted_date, cycle, location, directory, vars,working_directory, s3_list, noaa_path){
+    curr_year <- lubridate::year(forecasted_date)
+    curr_month <- lubridate::month(forecasted_date)
+    if(curr_month < 10) curr_month <- paste0("0",curr_month)
+    curr_day <- lubridate::day(forecasted_date)
+    if(curr_day < 10) curr_day <- paste0("0",curr_day)
+    start_date <- paste0(curr_year,curr_month,curr_day)
+    directory <- paste0("&dir=%2Fcfs.",start_date,"%2F",cycle,"%2F6hrly_grib_01")
+
+    date_vector <- seq(forecasted_date, forecasted_date + lubridate::days(217), "1 day")
+    forecast_hours <- c("00", "06", "12", "18")
+
+    if(cycle == "00"){
+      start_index = 1
+      end_index = 4
+    }
+    if(cycle == "06"){
+      start_index = 2
+      end_index = 1
+    }
+    if(cycle == "12"){
+      start_index = 3
+      end_index = 2
+    }
+    if(cycle == "18"){
+      start_index = 4
+      end_index = 3
+    }
+
+
+    for(ii in 1:length(date_vector)){
+
+      curr_year <- lubridate::year(date_vector[ii])
+      curr_month <- lubridate::month(date_vector[ii])
+      if(curr_month < 10) curr_month <- paste0("0",curr_month)
+      curr_day <- lubridate::day(date_vector[ii])
+      if(curr_day < 10) curr_day <- paste0("0",curr_day)
+      curr_date <- paste0(curr_year,curr_month,curr_day)
+
+
+      for(jj in 1:length(forecast_hours)){
+
+        if((ii == 1 & jj >= start_index) | (ii > 1 & ii < length(date_vector)) | (ii == length(date_vector) & jj <= end_index)){
+
+          file_name <- paste0("flxf",curr_date, forecast_hours[jj],".01.",start_date, cycle,".grb2")
+
+          #flxf2021072600.01.2021072600.grb2
+
+          destfile <- paste0(working_directory,"/", file_name,".",grid_name,".grib")
+
+          download_file <- FALSE
+          if(!s3_mode){
+            if(!file.exists(destfile)){
+              download_file <- TRUE
+            }
+          }else{
+            s3_file <- file.path(noaa_path, basename(destfile))
+            if(!(s3_file %in% s3_list)){
+              download_file <- TRUE
+            }
+          }
+
+          if(download_file){
+            download_tries <- 1
+            download_failed <- TRUE
+            while(download_failed & download_tries < 2){
+              Sys.sleep(1.0)
+              download_failed <- FALSE
+              out <- tryCatch(download.file(paste0(base_filename1, file_name, vars, location, directory),
+                                            destfile = destfile, quiet = TRUE),
+                              error = function(e){
+                                warning(paste(e$message, "skipping", file_name),
+                                        call. = FALSE)
+                                return(NA)
+                              },
+                              finally = NULL)
+
+              if(is.na(out) | file.info(destfile)$size == 0){
+                download_failed <- TRUE
+              }else{
+
+                #band 1: tmp (surface) - not used but is automatically included
+                #band 2: longwave (surface)
+                #band 3: shortwave (surface)
+                #band 4: rain (PRATE)
+                #band 5: uwind (10 m)
+                #band 6: v wind (10 m)
+                #band 7: Tmp (2m) used
+                #band 8: specific humidity (2 m)
+                #band 9: pressure (surface)
+
+
+                grib <- rgdal::readGDAL(destfile, silent = TRUE)
+                if(is.null(grib$band1) | is.null(grib$band5) | is.null(grib$band6) | is.null(grib$band7) | is.null(grib$band8) | is.null(grib$band5)){
+
+                  if(ii >= 1 | (ii == 1 & jj > 1) & (is.null(grib$band2) | is.null(grib$band3) | is.null(grib$band4))){
+                    unlink(destfile)
+                    download_failed <- TRUE
+                  }else{
+                    unlink(destfile)
+                    download_failed <- TRUE
+                  }
+                }
+
+                if(s3_mode & !download_failed){
+                  success_transfer <- aws.s3::put_object(file = destfile,
+                                                         object = file.path(noaa_path, basename(destfile)),
+                                                         bucket = bucket)
+                  if(success_transfer){
+                    unlink(destfile, force = TRUE)
+                  }
+                }
+              }
+              download_tries <- download_tries + 1
+            }
+          }
+        }
+      }
+    }
+  }
 
   if(length(which(lon_list > 180)) > 0){
     stop("longitude values need to be between -180 and 180")
@@ -90,16 +203,21 @@ noaa_cfs_grid_download <- function(lat_list,
       for(j in 1:length(forecast_hours)){
 
         if((is.na(forecast_time) | (forecast_hours[j] %in% as.numeric(forecast_time))) &
-           (forecasted_date < curr_date | (forecasted_date == curr_date & lubridate::hour(curr_time) < forecast_hours[j]))){
+           (forecasted_date < curr_date | ((lubridate::as_datetime(forecasted_date) + lubridate::hours(forecast_hours[j])) < curr_time))){
           cycle <- forecast_hours[j]
 
           if(cycle < 10) cycle <- paste0("0",cycle)
 
-          model_date_hour_dir <- file.path(model_name_raw,forecasted_date,cycle)
-          dir.create(file.path(output_directory, model_date_hour_dir), recursive=TRUE, showWarnings = FALSE)
+          model_date_hour_dir <- file.path(model_dir,forecasted_date,cycle)
+
+          noaa_path <- file.path(model_name_raw, forecasted_date,cycle)
+
+          if(!dir.exists(model_date_hour_dir)){
+            dir.create(model_date_hour_dir, recursive=TRUE, showWarnings = FALSE)
+          }
 
           if(s3_mode){
-            s3_objects <- aws.s3::get_bucket(bucket = bucket, prefix = model_date_hour_dir, max = Inf)
+            s3_objects <- aws.s3::get_bucket(bucket = bucket, prefix = noaa_path, max = Inf)
             s3_list<- vapply(s3_objects, `[[`, "", "Key", USE.NAMES = FALSE)
             empty <- grepl("/$", s3_list)
             s3_list <- s3_list[!empty]
@@ -113,103 +231,7 @@ noaa_cfs_grid_download <- function(lat_list,
 
             print(paste("Downloading", forecasted_date, cycle))
 
-            curr_year <- lubridate::year(forecasted_date)
-            curr_month <- lubridate::month(forecasted_date)
-            if(curr_month < 10) curr_month <- paste0("0",curr_month)
-            curr_day <- lubridate::day(forecasted_date)
-            if(curr_day < 10) curr_day <- paste0("0",curr_day)
-            start_date <- paste0(curr_year,curr_month,curr_day)
-            directory <- paste0("&dir=%2Fcfs.",start_date,"%2F",cycle,"%2F6hrly_grib_01")
-
-            date_vector <- seq(forecasted_date, forecasted_date + lubridate::days(217), "1 day")
-            forecast_hours <- c("00", "06", "12", "18")
-
-            if(cycle == "00"){
-              start_index = 1
-              end_index = 4
-            }
-            if(cycle == "06"){
-              start_index = 2
-              end_index = 1
-            }
-            if(cycle == "12"){
-              start_index = 3
-              end_index = 2
-            }
-            if(cycle == "18"){
-              start_index = 4
-              end_index = 3
-            }
-
-
-            for(ii in 1:length(date_vector)){
-
-              curr_year <- lubridate::year(date_vector[ii])
-              curr_month <- lubridate::month(date_vector[ii])
-              if(curr_month < 10) curr_month <- paste0("0",curr_month)
-              curr_day <- lubridate::day(date_vector[ii])
-              if(curr_day < 10) curr_day <- paste0("0",curr_day)
-              curr_date <- paste0(curr_year,curr_month,curr_day)
-
-
-              for(jj in 1:length(forecast_hours)){
-
-                if((ii == 1 & jj >= start_index) | (ii > 1 & ii < length(date_vector)) | (ii == length(date_vector) & jj <= end_index)){
-
-                  noaa_file_name <- paste0("flxf",curr_date, forecast_hours[jj],".01.",start_date, cycle,".grb2")
-
-                  file_name <- paste0(noaa_file_name,".",grid_name,".grib")
-
-                  #flxf2021072600.01.2021072600.grb2
-
-                  destfile <- file.path(output_directory, model_date_hour_dir, file_name)
-
-                  download_file <- FALSE
-                  if(!s3_mode){
-                    if(!file.exists(destfile)){
-                      download_file <- TRUE
-                    }
-                  }else{
-                    s3_file <- file.path(model_date_hour_dir, file_name)
-                    if(!(s3_file %in% s3_list)){
-                      download_file <- TRUE
-                    }
-                  }
-                  if(download_file){
-                    download_tries <- 1
-                    download_failed <- TRUE
-                    while(download_failed & download_tries <= 1){
-                      if(download_tries > 1) {
-                        Sys.sleep(5)
-                      }
-                      download_failed <- FALSE
-                      out <- tryCatch(download.file(paste0(base_filename1, noaa_file_name, vars, location, directory),
-                                                    destfile = destfile, quiet = TRUE),
-                                      error = function(e){
-                                        warning(paste(e$message, "skipping", file_name),
-                                                call. = FALSE)
-                                        return(NA)
-                                      },
-                                      finally = NULL)
-
-                      download_check <- suppressWarnings(Rnoaa4cast:::check_grib_file(file = destfile, hour = forecast_hours[jj]))
-                      if(download_check == FALSE){
-                        unlink(destfile, force = TRUE)
-                      }else{
-                        if(s3_mode){
-                          success_transfer <- aws.s3::put_object(file = destfile,
-                                                                 object = file.path(model_date_hour_dir, file_name),
-                                                                 bucket = bucket)
-                          if(success_transfer){
-                            unlink(destfile, force = TRUE)
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
+            download_grid(forecasted_date = forecasted_date, cycle, location, directory, vars,working_directory = model_date_hour_dir, s3_list = s3_list, noaa_path = noaa_path)
           }else{
             print(paste("Existing", forecasted_date, cycle))
           }
